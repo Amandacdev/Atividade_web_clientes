d36bd73c2b4703d437346c7408e10c3b2d0b1d6c
Amanda Cruz de Araújo <amanda.crz.aj@gmail.com>
2023-09-14 21:59:59 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 //let contaController = new ContaController();
 let clienteController = new ClienteController();
 
-
+/*
 contaController.listar();
 
 const c1 = new Conta('1', 100);
@@ -12,7 +12,11 @@ p1.atualizarSaldoAniversario();
 console.log('Poupanca: ' + p1.saldo);
 cb1.creditar(100);
 console.log('Conta Bonificada: ' + cb1.saldo);
+*/
 
 console.log('---------------------------------------');
 console.log('Testes para Clientes');
 clienteController.listar();
+
+clienteController.listar()
+
--- ts/model/cliente-especial.ts
+++ ts/model/cliente-especial.ts
@@ -1,7 +1,9 @@
 class ClienteEspecial extends Cliente {
+    private dependentes: Array<Cliente>;
 
     constructor(nome: string, cpf: string, conta: string) {
         super(nome,cpf,conta);
+        this.dependentes = new Array<Cliente>();
     }
-    
+
 }
